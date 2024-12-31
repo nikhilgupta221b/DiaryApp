d564ea3b8ae8a7e3ee6fb10b83472483e800134f
Nikhil <nikhilgupta304@gmail.com>
2025-01-01 01:08:18 +0530

    Add project files.

---
+++ DiaryApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,41 @@
+﻿using DiaryApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiaryApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        
+        public DbSet<DiaryEntry> DiaryEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<DiaryEntry>().HasData(
+                new DiaryEntry
+                {
+                    Id = 1,
+                    Title = "Went Hiking",
+                    Content = "Went hiking with Joe",
+                    Created = new DateTime(2025, 1, 1, 0, 0, 0)
+                },
+                new DiaryEntry
+                {
+                    Id = 2,
+                    Title = "Went Biking",
+                    Content = "Went biking with Joe",
+                    Created = new DateTime(2025, 1, 1, 0, 0, 0)
+                },
+                new DiaryEntry
+                {
+                    Id = 3,
+                    Title = "Went Shopping",
+                    Content = "Went shopping with Joe",
+                    Created = new DateTime(2025, 1, 1, 0, 0, 0)
+                }
+                );
+        }
+    }
+}
